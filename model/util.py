3ec6a8bb2ddc5951e3fe5191e87dcfc63e7fa85e
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2022-06-13 22:04:33 +0200

    added random id generator with comments

--- model/util.py
+++ model/util.py
@@ -7,4 +7,14 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+
+    random_small_letters = [random.choice(string.ascii_lowercase) for letter in range(number_of_small_letters)]
+    random_capital_letters = [random.choice(string.ascii_uppercase) for letter in range(number_of_capital_letters)]
+    random_digits = [random.choice(str(string.digits)) for number in range(number_of_digits)]
+    random_spec_chars = [random.choice(allowed_special_chars) for chars in range(number_of_special_chars)]
+    half_random_id = random_small_letters + random_capital_letters + random_digits + random_spec_chars # összeadja a random kiválaszott betűket és számokat és spec karaktereket egy listába rendezetten
+    random.shuffle(half_random_id) # összekeveri a random kiválasztott betűket, számokat és spec karaktereket 
+    random_id = ''.join(half_random_id)
+    return random_id    
+
+    # return 'T!uq6-b4Yq'
