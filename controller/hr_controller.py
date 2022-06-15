6a6d629813267b622062541c746820059622112b
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2022-06-15 13:28:58 +0200

    add print done

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -3,8 +3,10 @@ from view import terminal as view
 
 
 def list_employees():
+    data = hr.list_employes()
+    view.print_table(data)
     view.print_error_message("Not implemented yet.")
-
+list_employees()
 
 def add_employee():
     view.print_error_message("Not implemented yet.")
