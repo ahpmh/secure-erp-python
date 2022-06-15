0321cc6613f53318266257e7c73b096a82585639
marktamasko <25462245+marktamasko@users.noreply.github.com>
2022-06-15 15:42:28 +0200

    sales_update

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -13,3 +13,27 @@ from model import data_manager, util
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
 
+def get_data_from_file():
+    data = data_manager.read_table_from_file(DATAFILE)
+    data.insert(0, HEADERS)
+    return data
+
+def write_to_file(plus_data):
+    plus_data.pop(0)
+    data_manager.write_table_to_file(DATAFILE, plus_data)
+
+def get_id():
+    id = util.generate_id()
+    return id
+
+def add_transaction():
+    data = get_data_from_file()
+    new_transaction = []
+    id = get_id()
+    custumer_id = get_id()
+    product = input("Please type the product name: ")
+    date_time = input('Please type the transaction date (YYYY-MM-DD): ' )
+    price = float(input('Please type the price: '))
+
+    
+
